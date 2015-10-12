013b6ec7f4ee735541e904bafe6fbd6270b140bf
Mariani88 <pm_mariani@hotmail.com>
2015-10-12 12:30:59 -0300

    test verde para ubicar lancha o acorazado horizontalmente

--- ejercicio3/src/test/java/skeleton/HorizontalTest.java
+++ ejercicio3/src/test/java/skeleton/HorizontalTest.java
@@ -5,38 +5,62 @@ import org.junit.Test;
 
 public class HorizontalTest {
 
-	private Orientacion horizontal = new Horizontal ();
-	private Barco [][] matrizTablero = new Barco [4][4]; 
-	private CampoDeBatalla campo = new CampoDeBatalla ();
-	
+	private Orientacion horizontal = new Horizontal();
+	private Barco[][] matrizTablero = new Barco[4][4];
+	private CampoDeBatalla campo = new CampoDeBatalla();
+
 	@Test
-	public void ubicarBarcoDebeColocarDestructorEnTableroHorizontalmente (){
+	public void ubicarBarcoDebeColocarDestructorEnTableroHorizontalmente() {
 		int fila = 0;
-		int columna = 0;	
+		int columna = 0;
 		this.campo.construirLugarDeCombate(this.matrizTablero);
-		Barco destructor = new Destructor ();
-		
-		this.horizontal.ubica (destructor, this.matrizTablero, fila, columna);
-		
+		Barco destructor = new Destructor();
+
+		this.horizontal.ubica(destructor, this.matrizTablero, fila, columna);
+
 		Assert.assertEquals(destructor, this.matrizTablero[0][0]);
 		Assert.assertEquals(destructor, this.matrizTablero[0][1]);
 		Assert.assertEquals(destructor, this.matrizTablero[0][2]);
-		this.assertarNullDesde (0,3);
+		this.assertarNullDesde(0, 3);
 	}
-	
-	
-	
-	
+
+	@Test
+	public void ubicarBarcoDebeColocarAcorazadoEnTableroHorizontalmente() {
+		int fila = 0;
+		int columna = 0;
+		this.campo.construirLugarDeCombate(this.matrizTablero);
+		Barco acorazado = new Acorazado();
+
+		this.horizontal.ubica(acorazado, this.matrizTablero, fila, columna);
+
+		Assert.assertEquals(acorazado, this.matrizTablero[0][0]);
+		Assert.assertEquals(acorazado, this.matrizTablero[0][1]);
+		this.assertarNullDesde(0, 2);
+	}
+
+	@Test
+	public void ubicarBarcoDebeColocarLanchaEnTableroHorizontalmente() {
+		int fila = 0;
+		int columna = 0;
+		this.campo.construirLugarDeCombate(this.matrizTablero);
+		Barco lancha = new Lancha();
+
+		this.horizontal.ubica(lancha, this.matrizTablero, fila, columna);
+
+		Assert.assertEquals(lancha, this.matrizTablero[0][0]);
+		this.assertarNullDesde(0, 1);
+	}
+
 	private void assertarNullDesde(int fila, int columna) {
-		
-		for (int j = columna; j < this.matrizTablero[fila].length; j++ ){
+
+		for (int j = columna; j < this.matrizTablero[fila].length; j++) {
 			Assert.assertNull(this.matrizTablero[fila][j]);
 		}
-		
-		for (int i = fila + 1; i < this.matrizTablero.length; i++){
-			for (int j = 0; j < this.matrizTablero [i].length; j++){
+
+		for (int i = fila + 1; i < this.matrizTablero.length; i++) {
+			for (int j = 0; j < this.matrizTablero[i].length; j++) {
 				Assert.assertNull(this.matrizTablero[i][j]);
 			}
 		}
-	}	
+	}
 }
