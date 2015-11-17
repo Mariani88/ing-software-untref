44d1e98540da3c9c3f4423dec27d9b52c3ef178f
Mariani88 <pm_mariani@hotmail.com>
2015-11-17 00:48:17 -0300

    test vede para que carta haga su aporte al envido

--- ejercicio4/src/main/java/truco/Carta.java
+++ ejercicio4/src/main/java/truco/Carta.java
@@ -19,6 +19,8 @@ public class Carta {
 		return this.aporteAEnvido;
 	}
 
-
-
+	public int contribuirAlEnvido() {
+		
+		return this.paloDeCarta.acumularEnvido(this.aporteAEnvido);
+	}
 }
--- ejercicio4/src/test/java/truco/CartaTest.java
+++ ejercicio4/src/test/java/truco/CartaTest.java
@@ -3,8 +3,6 @@ package truco;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-
 public class CartaTest {
 
 	@Test
@@ -20,4 +18,22 @@ public class CartaTest {
 		Carta carta = new Carta (10, new Espada ());
 		Assert.assertEquals(0, carta.getAporteAEnvido () );
 	}
+	
+	@Test
+	public void contribuirAEnvidoDebeSumarAlEnvido (){
+		
+		Palo espada = new Espada ();
+		
+		Carta carta1 = new Carta (10, espada);
+		Carta carta2 = new Carta (7, espada);
+		
+		int envido = carta1.contribuirAlEnvido ();
+		envido = carta2.contribuirAlEnvido ();
+		
+		Assert.assertEquals(27, envido);
+		
+	}
+	
+	
+	
 }
