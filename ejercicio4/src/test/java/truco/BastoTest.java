21402572569dc644990b2fede80667db1c73be20
Mariani88 <pm_mariani@hotmail.com>
2015-11-17 00:15:31 -0300

    test verde para que basto calcule envido para dos cartas

--- ejercicio4/src/test/java/truco/BastoTest.java
+++ ejercicio4/src/test/java/truco/BastoTest.java
@@ -9,13 +9,27 @@ public class BastoTest {
 	@Test
 	public void envidoAcumuladoDebeDevolverEnvidoAcumuladoParaUnaCarta (){
 		
-		Palo espada = new Basto ();
+		Palo basto = new Basto ();
 		int aporteDeCarta1 = 7;
 		int envidoEsperado = aporteDeCarta1;
 		
-		int envidoAcumulado = espada.acumularEnvido ( aporteDeCarta1 );
+		int envidoAcumulado = basto.acumularEnvido ( aporteDeCarta1 );
 		Assert.assertEquals(envidoEsperado, envidoAcumulado);
 	}
 	
+	
+	@Test
+	public void envidoAcumuladoDebeDevolverEnvidoAcumuladoParaDosCartas (){
+		
+		Palo basto = new Basto ();
+		int aporteDeCarta1 = 7;
+		int aporteDeCarta2 = 6; 
+		int envidoEsperado = aporteDeCarta1 + aporteDeCarta2 + 20;
+		
+		int envidoAcumulado = basto.acumularEnvido ( aporteDeCarta1 );
+		envidoAcumulado = basto.acumularEnvido ( aporteDeCarta2 );
+		
+		Assert.assertEquals(envidoEsperado, envidoAcumulado);
+	}
 	
 }
